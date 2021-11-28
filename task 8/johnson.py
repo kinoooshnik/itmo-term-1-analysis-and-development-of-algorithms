cf2e89ccf7d7401046aed9ee4d6d96c967917b4e
Aleksandr Semiletov <alexander@semiletov.me>
2021-11-28 23:59:53 +0300

    Add task 8

---
+++ task 8/johnson.py
@@ -0,0 +1,39 @@
+import random
+import sys
+
+import networkx as nx
+import numpy as np
+from memory_profiler import profile
+
+
+def generate_graph(n):
+    m = (n * (n - 1)) // 2
+    weight_range = (3, 10)
+    matrix = np.zeros((n, n), dtype=int)
+    edges = 0
+    while edges < m:
+        a = random.randint(0, n - 1)
+        b = random.randint(0, n - 1)
+        if a == b or matrix[a, b] != 0:
+            continue
+        weight = random.randint(*weight_range)
+        matrix[a, b] = weight
+        matrix[b, a] = weight
+        edges += 1
+    G = nx.from_numpy_matrix(matrix)
+    return G
+
+
+def johnson(G):
+    return nx.johnson(G, weight="weight")
+
+
+@profile
+def main(n: int):
+    G = generate_graph(n)
+    johnson(G)
+
+
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+    main(n)
